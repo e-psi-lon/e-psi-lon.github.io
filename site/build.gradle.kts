47a0916791e8b4881078b84a806da53481fa0bb8
e-psi-lon <theg41g@gmail.com>
2024-08-26 20:32:20 +0200

    chore: Update to kotlin 2.0.10 and kobweb 0.19.0

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -1,9 +1,8 @@
-import com.varabyte.kobweb.gradle.application.extensions.AppBlock.LegacyRouteRedirectStrategy
 import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.plugin.compose)
     alias(libs.plugins.kobweb.application)
     alias(libs.plugins.kobwebx.markdown)
 }
@@ -19,7 +18,6 @@ kobweb {
 
         // Only legacy sites need this set. Sites built after 0.16.0 should default to DISALLOW.
         // See https://github.com/varabyte/kobweb#legacy-routes for more information.
-        legacyRouteRedirectStrategy.set(LegacyRouteRedirectStrategy.DISALLOW)
     }
 }
 
@@ -28,11 +26,11 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-            implementation(compose.runtime)
+            implementation(libs.compose.runtime)
         }
 
         jsMain.dependencies {
-            implementation(compose.html.core)
+            implementation(libs.compose.html.core)
             implementation(libs.kobweb.core)
             implementation(libs.kobweb.silk)
             implementation(libs.silk.icons.fa)
